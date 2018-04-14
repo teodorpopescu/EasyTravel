1f96612931402df5e5702f712d38f0b918b22dfe
Gabriel Boroghina <gabib97@yahoo.com>
2018-04-15 01:42:36 +0300

    Updated DashboardLayout JavaScript updates

--- EasyTravel/Scripts/location.js
+++ EasyTravel/Scripts/location.js
@@ -1,4 +1,5 @@
-﻿
+﻿var placeID = 'ChIJT608vzr5sUARKKacfOMyBqw';
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -33.8688, lng: 151.2195 },
@@ -41,8 +42,11 @@ function initMap() {
             placeId: place.place_id,
             location: place.geometry.location
         });
-        setForecast(place.place_id);
+
         marker.setVisible(true);
+
+        placeID = place.place_id; // update global variable
+        setForecast(place.place_id);
 
         infowindowContent.children['place-name'].textContent = place.name;
         infowindowContent.children['place-id'].textContent = place.place_id;
@@ -58,14 +62,24 @@ function getForecastUrl(place_id) {
     request.send(null);
     return request.responseText;
 }
+
 function setForecast(place_id) {
-    var d = document;
-    s = "script";
-    id = "weatherwidget-io-js";
-   // var js, fjs = d.getElementsByTagName(s)[0];
-    var wtf = getForecastUrl(place_id);
-    var x = d.getElementById('weather-widget');
-    x.setAttribute("data-label_1", wtf.split("/")[1].toUpperCase());
-    x.href = "https://forecast7.com/en/" + wtf + "/";
-  //  if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs);}
+    var js, fjs = document.getElementById('weather_script');
+
+    var id = 'weatherwidget-io-js';
+    var location = getForecastUrl(place_id);
+
+    var x = document.getElementById('weather-widget');
+
+    x.setAttribute("data-label_1", location.split("/")[1].toUpperCase());
+    x.href = "https://forecast7.com/en/" + location + "/";
+
+    // restart the weather widget
+    document.getElementById(id).remove();
+    if (!document.getElementById(id)) {
+        js = document.createElement('script');
+        js.id = id;
+        js.src = 'https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }
 }
